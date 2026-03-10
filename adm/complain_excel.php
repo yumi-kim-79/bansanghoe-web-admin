318bf51d9821dcdb909619bb26835f866873d6a8
yumi-kim-79 <routefinding2025@gmail.com>
2026-03-10 12:02:11 +0900

    Update complain_excel.php

--- adm/complain_excel.php
+++ adm/complain_excel.php
@@ -41,35 +41,33 @@ $sql = "SELECT
         WHERE complain.complain_idx IN ({$inClause})
         ORDER BY complain.complain_idx DESC";
 
-$result = sql_query($sql);
-$rows = [];
-while ($row = sql_fetch_array($result)) {
-    $rows[] = $row;
+$query_result = sql_query($sql);  // ← $rows 변수명 충돌 방지
+$data_rows = [];                  // ← $rows 대신 $data_rows 사용
+while ($row = sql_fetch_array($query_result)) {
+    $data_rows[] = $row;
 }
 
-$headers = ['번호','지역','단지명','동','호수','접수날짜','민원인','연락처','작성자','민원 제목','민원 내용','민원 답변','추가 내용','담당자 부서','담당자','완료날짜','상태'];
+function col_name($idx) {
+    $letters = ''; $idx++;
+    while ($idx > 0) { $idx--; $letters = chr(65 + ($idx % 26)) . $letters; $idx = intval($idx / 26); }
+    return $letters;
+}
 
-// xlsx 는 zip 구조이므로 직접 생성
-$filename = '민원_' . date('Ymd_His') . '.xlsx';
-$tmpFile = tempnam(sys_get_temp_dir(), 'complain_xlsx_');
-
-// XML 이스케이프 함수
 function xe($v) {
     return htmlspecialchars((string)$v, ENT_XML1 | ENT_QUOTES, 'UTF-8');
 }
 
-// 공유 문자열 방식 없이 인라인 문자열(t="inlineStr")로 작성
-$rowsXml = '';
-// 헤더행
-$rowsXml .= '<row r="1">';
-foreach ($headers as $ci => $h) {
-    $col = col_name($ci) . '1';
-    $rowsXml .= '<c r="'.$col.'" t="inlineStr"><is><t>' . xe($h) . '</t></is></c>';
+$excel_headers = ['번호','지역','단지명','동','호수','접수날짜','민원인','연락처','작성자','민원 제목','민원 내용','민원 답변','추가 내용','담당자 부서','담당자','완료날짜','상태'];
+
+// 헤더행 XML
+$rowsXml = '<row r="1">';
+foreach ($excel_headers as $ci => $h) {
+    $rowsXml .= '<c r="'.col_name($ci).'1" t="inlineStr"><is><t>' . xe($h) . '</t></is></c>';
 }
 $rowsXml .= '</row>';
 
-// 데이터행
-foreach ($rows as $ri => $row) {
+// 데이터행 XML
+foreach ($data_rows as $ri => $row) {
     $r = $ri + 2;
     $cells = [
         $row['complain_idx'],
@@ -90,26 +88,18 @@ foreach ($rows as $ri => $row) {
         $row['edate'],
         $row['status_name'],
     ];
+
     $rowsXml .= '<row r="'.$r.'">';
     foreach ($cells as $ci => $val) {
-        $col = col_name($ci) . $r;
-        $rowsXml .= '<c r="'.$col.'" t="inlineStr"><is><t>' . xe($val) . '</t></is></c>';
+        $rowsXml .= '<c r="'.col_name($ci).$r.'" t="inlineStr"><is><t>' . xe($val) . '</t></is></c>';
     }
     $rowsXml .= '</row>';
 }
 
-function col_name($idx) {
-    $letters = '';
-    $idx++;
-    while ($idx > 0) {
-        $idx--;
-        $letters = chr(65 + ($idx % 26)) . $letters;
-        $idx = intval($idx / 26);
-    }
-    return $letters;
-}
+// xlsx zip 생성
+$excel_filename = '민원_' . date('Ymd_His') . '.xlsx';
+$tmpFile = tempnam(sys_get_temp_dir(), 'complain_xlsx_');
 
-// xlsx 파일 구성 (zip)
 $zip = new ZipArchive();
 $zip->open($tmpFile, ZipArchive::OVERWRITE);
 
@@ -132,9 +122,7 @@ $zip->addFromString('xl/workbook.xml',
 '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"
           xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
-  <sheets>
-    <sheet name="민원목록" sheetId="1" r:id="rId1"/>
-  </sheets>
+  <sheets><sheet name="민원목록" sheetId="1" r:id="rId1"/></sheets>
 </workbook>');
 
 $zip->addFromString('xl/_rels/workbook.xml.rels',
@@ -153,7 +141,7 @@ $zip->close();
 
 $filesize = filesize($tmpFile);
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment; filename="' . $filename . '"');
+header('Content-Disposition: attachment; filename="' . $excel_filename . '"');
 header('Content-Length: ' . $filesize);
 header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 header('Pragma: public');
