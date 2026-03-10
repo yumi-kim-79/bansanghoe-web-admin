953966397076376eb50a6eab8bb5bcab350ec406
yumi-kim-79 <routefinding2025@gmail.com>
2026-03-10 11:33:41 +0900

    Update complain_excel.php

--- adm/complain_excel.php
+++ adm/complain_excel.php
@@ -1,18 +1,13 @@
 <?php
 /**
- * complain_excel.php - 민원 엑셀 다운로드
- * _common.php 의 HTML 출력을 무력화하기 위해 파일로 먼저 생성 후 전송
+ * complain_excel.php - 민원 엑셀 다운로드 (.xlsx)
  */
-
-// output buffering 시작 (common.php HTML 출력 차단)
 ob_start();
 require_once './_common.php';
-ob_end_clean(); // common.php 가 출력한 모든 HTML 버림
+ob_end_clean();
 
 $idxList = isset($_POST['idx']) ? $_POST['idx'] : [];
-if (empty($idxList)) {
-    die('선택된 항목이 없습니다.');
-}
+if (empty($idxList)) die('선택된 항목이 없습니다.');
 
 $idxList = array_map('intval', $idxList);
 $inClause = implode(',', $idxList);
@@ -52,18 +47,31 @@ while ($row = sql_fetch_array($result)) {
     $rows[] = $row;
 }
 
-// 임시 파일에 먼저 CSV 내용 작성
-$tmpFile = tempnam(sys_get_temp_dir(), 'complain_excel_');
-$fp = fopen($tmpFile, 'w');
-
-// UTF-8 BOM
-fwrite($fp, "\xEF\xBB\xBF");
-
 $headers = ['번호','지역','단지명','동','호수','접수날짜','민원인','연락처','작성자','민원 제목','민원 내용','민원 답변','추가 내용','담당자 부서','담당자','완료날짜','상태'];
-fputcsv($fp, $headers, "\t");
 
-foreach ($rows as $row) {
-    $line = [
+// xlsx 는 zip 구조이므로 직접 생성
+$filename = '민원_' . date('Ymd_His') . '.xlsx';
+$tmpFile = tempnam(sys_get_temp_dir(), 'complain_xlsx_');
+
+// XML 이스케이프 함수
+function xe($v) {
+    return htmlspecialchars((string)$v, ENT_XML1 | ENT_QUOTES, 'UTF-8');
+}
+
+// 공유 문자열 방식 없이 인라인 문자열(t="inlineStr")로 작성
+$rowsXml = '';
+// 헤더행
+$rowsXml .= '<row r="1">';
+foreach ($headers as $ci => $h) {
+    $col = col_name($ci) . '1';
+    $rowsXml .= '<c r="'.$col.'" t="inlineStr"><is><t>' . xe($h) . '</t></is></c>';
+}
+$rowsXml .= '</row>';
+
+// 데이터행
+foreach ($rows as $ri => $row) {
+    $r = $ri + 2;
+    $cells = [
         $row['complain_idx'],
         $row['post_name'],
         $row['building_name'],
@@ -82,15 +90,69 @@ foreach ($rows as $row) {
         $row['edate'],
         $row['status_name'],
     ];
-    fputcsv($fp, $line, "\t");
+    $rowsXml .= '<row r="'.$r.'">';
+    foreach ($cells as $ci => $val) {
+        $col = col_name($ci) . $r;
+        $rowsXml .= '<c r="'.$col.'" t="inlineStr"><is><t>' . xe($val) . '</t></is></c>';
+    }
+    $rowsXml .= '</row>';
 }
-fclose($fp);
 
-$filename = '민원_' . date('Ymd_His') . '.xls';
+function col_name($idx) {
+    $letters = '';
+    $idx++;
+    while ($idx > 0) {
+        $idx--;
+        $letters = chr(65 + ($idx % 26)) . $letters;
+        $idx = intval($idx / 26);
+    }
+    return $letters;
+}
+
+// xlsx 파일 구성 (zip)
+$zip = new ZipArchive();
+$zip->open($tmpFile, ZipArchive::OVERWRITE);
+
+$zip->addFromString('[Content_Types].xml',
+'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+  <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
+  <Default Extension="xml" ContentType="application/xml"/>
+  <Override PartName="/xl/workbook.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"/>
+  <Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/>
+</Types>');
+
+$zip->addFromString('_rels/.rels',
+'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="xl/workbook.xml"/>
+</Relationships>');
+
+$zip->addFromString('xl/workbook.xml',
+'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"
+          xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
+  <sheets>
+    <sheet name="민원목록" sheetId="1" r:id="rId1"/>
+  </sheets>
+</workbook>');
+
+$zip->addFromString('xl/_rels/workbook.xml.rels',
+'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/>
+</Relationships>');
+
+$zip->addFromString('xl/worksheets/sheet1.xml',
+'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+  <sheetData>' . $rowsXml . '</sheetData>
+</worksheet>');
+
+$zip->close();
+
 $filesize = filesize($tmpFile);
-
-// 헤더 전송 후 파일 출력
-header('Content-Type: application/vnd.ms-excel; charset=UTF-8');
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Disposition: attachment; filename="' . $filename . '"');
 header('Content-Length: ' . $filesize);
 header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
